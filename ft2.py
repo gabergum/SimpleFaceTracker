eed1c71855f8d3d6a4ee3fc9e59fc9a705521f32
gabergum <43650407+gabergum@users.noreply.github.com>
2020-05-28 08:46:39 -0700

    Update ft2.py

--- ft2.py
+++ ft2.py
@@ -1,22 +1,15 @@
 import cv2
-import sys
-
-
 cascPath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
 ds_factor=0.6
 class VideoCamera(object):
 	def __init__(self):
 		self.video = cv2.VideoCapture(0)
-
 	def __dell__(self):
 		self.video.release()
-
 	def get_frame(self):
 		ret, frame = self.video.read()
-
 		frame=cv2.resize(frame,None,fx=ds_factor,fy=ds_factor,interpolation=cv2.INTER_AREA)
-		
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		faces = faceCascade.detectMultiScale(
 			gray,
@@ -24,10 +17,8 @@ class VideoCamera(object):
 			minNeighbors=5,
 			minSize=(30,30),
 		)
-
 		for (x,y,w,h) in faces:
 			cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-
 		ret, jpeg = cv2.imencode('.jpg',frame)
 		return jpeg.tobytes()
 
